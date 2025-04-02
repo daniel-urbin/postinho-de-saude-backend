13bedeaa3d326ac2e2d67acca401c6b509942693
daniel-urbin <daniel.urbin@acad.ftec.com.br>
2025-04-01 22:43:41 -0300

    criando endpoint de usuario, ainda não tem a scructura

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,16 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World from TypeScript API!');
 });
 
+app.get('/usuario',(req: Request, res: Response) => {
+  const usuario = {
+    id: 1,
+    nome: 'Joao',
+    email: 'teste@teste.com'
+  }
+
+  res.json(usuario);
+});
+
 // Esta parte é crucial para o Vercel saber qual porta usar
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
