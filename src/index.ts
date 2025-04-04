6e7f0f6bb2dafe8d36cc63e5a1954277947ce0bb
daniel-urbin <daniel.urbin@acad.ftec.com.br>
2025-04-04 01:31:55 -0300

    já é tarde a dormir por hoje deu

--- src/index.ts
+++ src/index.ts
@@ -42,6 +42,24 @@ app.post('/usuario', (req: Request, res: Response) => {
   res.status(201).json(newUser);
 });
 
+
+// Endpoint para eliminar un usuario
+// app.delete("/usuario/:id", (req: Request, res: Response) => {
+//     //const { id } = parseInt(req.params.id);
+//     const id: any = req.params.id;
+//     const index = usuarios.findIndex(usuario => usuario.id === parseInt(id)); // Convertir id a número
+
+//     if (index !== -1) {
+//         usuarios.splice(index, 1);
+//         return res.status(200).send({ message: 'Usuario eliminado con éxito' });
+//     } else {
+//         return res.status(404).send({ message: 'Usuario no encontrado' });
+//     }
+// });
+
+
+
+
 // Esta parte es crucial para el Vercel saber cuál puerto usar
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
