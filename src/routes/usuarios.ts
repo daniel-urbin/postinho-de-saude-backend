11807268515d5712ec9a7e0d5aaa26a52fd54feb
daniel-urbin <daniel.urbin@acad.ftec.com.br>
2025-04-06 14:09:38 -0300

    falatava criptografar a senha ao fazer inser ou update

--- src/routes/usuarios.ts
+++ src/routes/usuarios.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import supabase from '../supabase.config';
+import bcrypt from 'bcryptjs';
 
 const router = express.Router();
 
@@ -40,20 +41,23 @@ router.get("/usuario/:id", async (req: Request, res: Response) => {
   }
 })
 
+
+
+// ...
+
 // Endpoint para agregar un nuevo usuario
 router.post('/usuario', async (req: Request, res: Response) => {
   try {
     const { nome, email, senha, cpf, telefone, dataNascimento, endereco } = req.body;
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
     const { data, error } = await supabase
       .from('usuario')
-      .insert([{ nome, email, senha, cpf, telefone, dataNascimento, endereco }]);
+      .insert([{ nome, email, senha: senhaCriptografada, cpf, telefone, dataNascimento, endereco }]);
 
     if (error) {
       res.status(500).send({ mensagem: "Erro ao inserir o usuário" });
     }
     else if (data === null) {
-      //tive que deixar assim por agora já que esta fucnioando,
-      //mais mesmo dando mensagem de erro vai criar 
       res.status(201).send({ mensagem: "Usuário criado com sucesso" });
     } else {      
       res.status(201).json(data[0]);
@@ -68,17 +72,26 @@ router.put("/usuario/:id", async (req: Request, res: Response) => {
   try {
     const id = parseInt(req.params.id);
     const { nome, email, senha, cpf, telefone, dataNascimento, endereco } = req.body;
+    let senhaCriptografada;
+    if (senha) {
+      senhaCriptografada = await bcrypt.hash(senha, 10);
+    }
     const { data, error } = await supabase
       .from('usuario')
-      .update({ nome: nome, email: email, senha: senha, cpf: cpf, telefone: telefone, dataNascimento: dataNascimento, endereco: endereco })
+      .update({ 
+        nome, 
+        email, 
+        senha: senhaCriptografada || undefined, 
+        cpf, 
+        telefone, 
+        dataNascimento, 
+        endereco 
+      })
       .eq('id', id);
-
 
     if (error) {
       res.status(500).send({ mensagem: "Erro ao atualizar o usuário" });
     } else if (data === null) {
-      //tive que deixar assim por agora já que esta fucnioando,
-      //mais mesmo dando mensagem de erro vai fazer update 
       res.status(200).send({mensagem: "Usuário atualizado com sucesso"})
     } else {
       res.json(data[0]);
@@ -87,6 +100,7 @@ router.put("/usuario/:id", async (req: Request, res: Response) => {
     res.status(500).send({ mensagem: "Erro ao atualizar o usuário" });
   }
 })
+
 
 // DELETE /usuario/:id
 router.delete("/usuario/:id", async (req: Request, res: Response) => {
