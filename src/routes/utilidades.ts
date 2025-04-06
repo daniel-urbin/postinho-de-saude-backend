48757300f278f84aa0c801bdbfb629cfca1a530a
daniel-urbin <daniel.urbin@acad.ftec.com.br>
2025-04-05 21:56:44 -0300

    teste simplificado de listado de endpoints

--- src/routes/utilidades.ts
+++ src/routes/utilidades.ts
@@ -1,23 +1,14 @@
 import express, { Request, Response, Router } from 'express';
 
-interface Layer {
-  route: {
-    path: string;
-    stack: { method: string }[];
-  };
-}
-
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-  const routes = (router as any).stack.filter((layer: Layer) => layer.route);
+let listadoEndpoint: string;
+listadoEndpoint = 'teste aqui vou listar os endpoints';
 
-  res.send(`
-    <h1>Endpoints disponíveis</h1>
-    <ul>
-      ${routes.map((layer: Layer) => `<li><strong>${layer.route.stack[0].method}</strong> ${layer.route.path}</li>`).join('')}
-    </ul>
-  `);
+
+router.get('/', (req: Request, res: Response) => {
+  res.send(listadoEndpoint);
 });
+
 
 export default router;
