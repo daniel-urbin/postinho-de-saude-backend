c2b8e0d7087be9b2694710d0f9698e2aa606bb78
daniel-urbin <daniel.urbin@acad.ftec.com.br>
2025-04-05 22:02:52 -0300

    monte o listado dos enpoint a ver como fica

--- src/routes/utilidades.ts
+++ src/routes/utilidades.ts
@@ -2,12 +2,22 @@ import express, { Request, Response, Router } from 'express';
 
 const router = express.Router();
 
-let listadoEndpoint: string;
-listadoEndpoint = 'teste aqui vou listar os endpoints';
+const endpoints = `
+<h1>Endpoints disponíveis</h1>
+<ul>
+  <li><strong>POST</strong> /login</li>
+  <li><strong>GET</strong> /protected</li>
+  <li><strong>GET</strong> /usuarios</li>
+  <li><strong>GET</strong> /usuario/:id</li>
+  <li><strong>POST</strong> /usuario</li>
+  <li><strong>PUT</strong> /usuario/:id</li>
+  <li><strong>DELETE</strong> /usuario/:id</li>
+</ul>
+`;
 
 
 router.get('/', (req: Request, res: Response) => {
-  res.send(listadoEndpoint);
+  res.send(endpoints);
 });
 
 
