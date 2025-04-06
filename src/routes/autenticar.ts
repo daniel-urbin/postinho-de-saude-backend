ddd337e372cb32bf2ca90842eb4c6c8975840ba7
daniel-urbin <daniel.urbin@acad.ftec.com.br>
2025-04-06 13:47:02 -0300

    implementada geração de token pegando o usuario desde a base

--- src/routes/autenticar.ts
+++ src/routes/autenticar.ts
@@ -1,17 +1,55 @@
 import express, { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import supabase from '../supabase.config';
 
 const router = express.Router();
 
-router.post('/login', (req: Request, res: Response) => {
-  const { username, password } = req.body;
+router.post('/login', async (req: Request, res: Response) => {
+  try {
+    const { nome, senha } = req.body;
 
-  // Simulação de autenticação (substitua por sua lógica de autenticação real)
-  if (username === 'admin' && password === 'password') {
-    const token = jwt.sign({ username }, process.env.SECRET_KEY!, { expiresIn: '1h' });
-    res.json({ token });
-  } else {
-    res.status(401).json({ error: 'Credenciais inválidas' });
+    const { data, error } = await supabase
+      .from('usuario')
+      .select('*')
+      .eq('nome', nome);
+
+    if (error) {
+      res.status(500).send({ mensagem: "Erro ao ler a tabela 'usuario'" });
+      return;
+    }
+
+    if (!data || data.length === 0) {
+      res.status(401).json({ error: 'Credenciais inválidas' });
+      return;
+    }
+
+    const user = data[0];
+
+    const passwordMatch = await bcrypt.compare(senha, user.senha);
+
+    if (!passwordMatch) {
+      res.status(422).json({ error: 'Nome ou senha incorreto!' });
+      return;
+    }
+
+    const token = jwt.sign({ nome: user.nome }, process.env.SECRET_KEY!, {
+      expiresIn: '1h',
+    });
+
+    const refreshToken = jwt.sign({ nome: user.nome }, process.env.SECRET_KEY!, {
+      expiresIn: '30d',
+    });
+
+    res.json({
+      user: {
+        name: user.nome,
+      },
+      token,
+      refresh_token: refreshToken,
+    });
+  } catch (error) {
+    res.status(500).send({ mensagem: "Erro ao autenticar o usuário" });
   }
 });
 
@@ -23,7 +61,7 @@ router.get('/protected', (req: Request, res: Response) => {
   } else {
     try {
       const decoded = jwt.verify(token, process.env.SECRET_KEY!) as jwt.JwtPayload;
-      res.json({ message: `Olá, ${decoded.username}!` });
+      res.json({ message: `Olá, ${decoded.nome}!` });
     } catch (error) {
       res.status(401).json({ error: 'Token inválido' });
     }
