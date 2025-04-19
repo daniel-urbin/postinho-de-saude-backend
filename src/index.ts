d9e36d6c5a146a72c9f3f249c1a8f9c11406cae2
daniel-urbin <daniel.urbin@acad.ftec.com.br>
2025-04-19 19:22:21 -0300

    adicionada nova rota

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,19 @@ import express, { Request, Response } from 'express';
 import usuariosRouter from './routes/usuarios';
 import autenticarRouter from './routes/autenticar';
 import utilidadesRouter from './routes/apiInfo';
+import zohoVerifyRouter from './routes/zohoVerify'; // Importar a nova rota
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-// Middleware para parsear el cuerpo de las solicitudes
+// Middleware para parsear o corpo das solicitações
 app.use(express.json());
 app.use(usuariosRouter);
 app.use(autenticarRouter);
 app.use(utilidadesRouter);
+app.use(zohoVerifyRouter); // Registrar a nova rota
 
-// Esta parte es crucial para el Vercel saber cuál puerto usar
+// Esta parte é crucial para o Vercel saber qual porta usar
 app.listen(port, () => {
   console.log(`Exemplo app escutando porta ${port}`);
 });
