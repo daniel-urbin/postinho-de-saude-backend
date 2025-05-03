781889efe2a4462e863e08f755b93254609975fd
daniel-urbin <daniel.urbin@acad.ftec.com.br>
2025-05-03 10:42:39 -0300

    removido endpoint de teste

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -7,7 +7,7 @@ import { enviarEmail } from '../helpers/emailHelper';
 const router = express.Router();
 
 // Endpoint de login
-router.post('/login_teste', async (req: Request, res: Response) => {
+router.post('/login', async (req: Request, res: Response) => {
   try {
     const { email, password: senha } = req.body;
 
@@ -34,66 +34,6 @@ router.post('/login_teste', async (req: Request, res: Response) => {
       res.status(422).json({ error: 'Email ou senha incorretos!' });
       return;
     }
-
-    const token = jwt.sign({ email: user.email }, process.env.SECRET_KEY!, {
-      expiresIn: '30d',
-    });
-
-    // Retornar os campos esperados no formato correto
-    res.json({
-      token,
-      user: {
-        id: user.id,
-        name: user.nome,
-        email: user.email,
-        phone: user.telefone,
-        birthdate: user.dataNascimento,
-        document: user.cpf,
-        role: user.tipo,
-        address: user.endereco,
-        admin: user.tipo === 'admin' ? user.id : null,
-        doctor: user.tipo === 'medico' ? user.id : null,
-        patient: user.tipo === 'paciente' ? user.id : null,
-        createdAt: user.created_at, // Certifique-se de que o campo existe no banco
-        updatedAt: user.updated_at, // Certifique-se de que o campo existe no banco
-      },
-    });
-  } catch (error) {
-
-    res.status(500).send({ mensagem: "Erro ao autenticar o usuário" });
-  }
-});
-
-
-
-// Endpoint de login
-router.post('/login', async (req: Request, res: Response) => {
-  try {
-    const { email, password: senha } = req.body;
-
-    const { data, error } = await supabase
-      .from('usuario')
-      .select('*')
-      .eq('email', email);
-
-    if (error) {
-      res.status(500).send({ mensagem: "Erro ao acessar a tabela 'usuario'" });
-      return;
-    }
-
-    if (!data || data.length === 0) {
-      res.status(401).json({ error: 'Credenciais inválidas' });
-      return;
-    }
-
-    const user = data[0];
-    const passwordMatch = await bcrypt.compare(senha, user.senha);
-
-
-//    if (!passwordMatch) {
-//      res.status(422).json({ error: 'Email ou senha incorretos!' });
-//      return;
-//    }
 
     const token = jwt.sign({ email: user.email }, process.env.SECRET_KEY!, {
       expiresIn: '30d',
